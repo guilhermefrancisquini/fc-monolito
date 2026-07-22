be02aa8bafe9aa1b665fc959ced526640d79a331
Guilherme Francisquini <guilherme.francisquini@voiter.com>
2026-07-22 20:36:23 -0300

    feat: adiciona rota para cadastrar produtos

---
+++ src/modules/product-adm/routes/products.post.route.spec.ts
@@ -0,0 +1,57 @@
+import { Sequelize } from "sequelize-typescript"
+import express, { Express } from 'express'
+import request from 'supertest'
+import { migrator } from "../../../databases/sequelize/config/migrator"
+import { productCreateRoute } from "./products.post.route"
+import { Umzug } from "umzug"
+import { ProductModel } from "../repository/product.model";
+
+
+describe("Products tests", () => {
+    const app: Express = express()
+    app.use(express.json())
+    app.use("/products", productCreateRoute)
+
+    let sequelize: Sequelize
+
+    let migration: Umzug<any>;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: ":memory:",
+        logging: false
+        })
+        
+        sequelize.addModels([ProductModel])
+        migration = migrator(sequelize)
+        await migration.up()
+    })
+
+    afterEach(async () => {
+        if (!migration || !sequelize) {
+            return 
+        }
+        migration = migrator(sequelize)
+        await migration.down()
+        await sequelize.close()
+    })
+
+    it("should create product registration", async () => {
+        const response = await request(app).post("/products").send({
+            id: "1",
+            name: "Product 1",
+            description: "Product 1 description",
+            purchasePrice: 10,
+            stock: 100
+        })
+
+        expect(response.status).toBe(201);
+        expect(response.body.id).toBe("1")
+        expect(response.body.name).toBe("Product 1")
+        expect(response.body.description).toBe("Product 1 description")
+        expect(response.body.purchasePrice).toBe(10)
+        expect(response.body.stock).toBe(100)
+    })
+
+})
---
+++ src/modules/product-adm/routes/products.post.route.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express"
+import ProductAdmFacadeFactory from "../factory/facade.factory";
+import { AddProductFacadeInputDto } from "../facade/product-adm.facade.interface";
+
+export const productCreateRoute = express.Router()
+
+productCreateRoute.post("/", async (req: Request, res: Response) => {
+  const { name, description, salesPrice } = req.body
+
+  try {
+    const productFacade = ProductAdmFacadeFactory.create()
+
+    const productAddDto: AddProductFacadeInputDto = {
+        id: req.body.id,
+        name: req.body.name,
+        description: req.body.description,
+        purchasePrice: req.body.purchasePrice,
+        stock: req.body.stock
+    }
+
+    const output = await productFacade.addProduct(productAddDto)
+
+    res.status(201).send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
