a0a6a8e9642fbcb1026daca977b8ea8a9d236d7a
Guilherme Francisquini <guilherme.francisquini@voiter.com>
2026-07-23 17:51:13 -0300

    feat: adiciona migrations de orders e order-products

--- src/databases/sequelize/migrations/2026.07.22T21.56.28.initial.ts
+++ src/databases/sequelize/migrations/2026.07.22T21.56.28.initial.ts
@@ -202,11 +202,107 @@ export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
 			allowNull: false,
 		},
 	});
+
+	await queryInterface.createTable("orders", {
+		id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			primaryKey: true,
+		},
+		client_id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_street: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_number: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_complement: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_city: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_state: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		client_zipCode: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		total: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		status: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		updated_at: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+	});
+
+	await queryInterface.createTable("order_products", {
+		id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			primaryKey: true,
+		},
+		order_id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			references: {
+				model: "orders",
+				key: "id",
+			},
+			onUpdate: "CASCADE",
+			onDelete: "CASCADE",
+		},
+		product_id: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		product_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		product_description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		product_sales_price: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	});
 };
 
 export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
 	const queryInterface = sequelize.getQueryInterface();
 
+	await queryInterface.dropTable("order_products");
+	await queryInterface.dropTable("orders");
 	await queryInterface.dropTable("transactions");
 	await queryInterface.dropTable("invoice_item");
 	await queryInterface.dropTable("invoice");
